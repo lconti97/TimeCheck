9427655dbf78c0ba701a3af4e6cdfdd719e4f022
lconti97 <lconti97@gmail.com>
2016-01-30 00:42:07 -0500

    add fabric and activities

--- app/src/main/java/com/hoya/vt/timecheck/MainActivity.java
+++ app/src/main/java/com/hoya/vt/timecheck/MainActivity.java
@@ -2,12 +2,22 @@ package com.hoya.vt.timecheck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "75YiknXrR89hvbnIsqK3HUipp";
+    private static final String TWITTER_SECRET = "lmUpO2m5m2Rd08klkPWPey4TDQAV9fKpwz7l13yfXrTIvoftcA";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/hoya/vt/timecheck/RiderActivity.java
@@ -0,0 +1,13 @@
+package com.hoya.vt.timecheck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RiderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rider);
+    }
+}
