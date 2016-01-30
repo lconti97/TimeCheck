9fabb0b88e958b8964f0e5cdaf49ea56b5ed2a52
Dag <dagmawi@vt.edu>
2016-01-30 01:34:23 -0500

    Added Twitter Hashtag Feed to Driver

--- app/src/main/java/com/hoya/vt/timecheck/DriverActivity.java
+++ app/src/main/java/com/hoya/vt/timecheck/DriverActivity.java
@@ -1,13 +1,33 @@
 package com.hoya.vt.timecheck;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DriverActivity extends AppCompatActivity {
+import com.twitter.sdk.android.tweetui.SearchTimeline;
+import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
+
+public class DriverActivity extends ListActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "75YiknXrR89hvbnIsqK3HUipp";
+    private static final String TWITTER_SECRET = "lmUpO2m5m2Rd08klkPWPey4TDQAV9fKpwz7l13yfXrTIvoftcA";
+
+    private static final String SEARCH_QUERY = "#Trump";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver);
+
+
+        SearchTimeline searchTimeline = new SearchTimeline.Builder()
+                .query(SEARCH_QUERY)
+                .build();
+
+        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
+                .setTimeline(searchTimeline)
+                .build();
+        setListAdapter(adapter);
     }
 }
