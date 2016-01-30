c7e9548acb4c6ca11af961ba64ac1ebc455af039
lconti97 <lconti97@gmail.com>
2016-01-30 16:43:26 -0500

    Revert "F this"
    This reverts commit 5c5dde9c0019adcbb31ba2af5a79dcbd86981973.

--- app/src/main/java/com/hoya/vt/timecheck/DriverActivity.java
+++ app/src/main/java/com/hoya/vt/timecheck/DriverActivity.java
@@ -1,82 +1,13 @@
 package com.hoya.vt.timecheck;
 
-import android.app.ListActivity;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
-import com.twitter.sdk.android.tweetui.SearchTimeline;
-import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
-
-public class DriverActivity extends ListActivity {
-
-    Toolbar toolbar;
-    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
-    private static final String TWITTER_KEY = "75YiknXrR89hvbnIsqK3HUipp";
-    private static final String TWITTER_SECRET = "lmUpO2m5m2Rd08klkPWPey4TDQAV9fKpwz7l13yfXrTIvoftcA";
-
-    private static final String SEARCH_QUERY = "#Trump";
+public class DriverActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver);
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-
-        // Set an OnMenuItemClickListener to handle menu item clicks
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                onOptionsItemSelected(item);
-                return true;
-            }
-        });
-
-        toolbar.inflateMenu(R.menu.menu_settings);
-
-        SearchTimeline searchTimeline = new SearchTimeline.Builder()
-                .query(SEARCH_QUERY)
-                .build();
-
-        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
-                .setTimeline(searchTimeline)
-                .build();
-        setListAdapter(adapter);
-    }
-
-    public void launchSettings() {
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
-        finish();
-        //test change
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.settingsMenu) {
-            launchSettings();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/hoya/vt/timecheck/MainActivity.java
+++ app/src/main/java/com/hoya/vt/timecheck/MainActivity.java
@@ -1,24 +1,12 @@
 package com.hoya.vt.timecheck;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button busButton;
-    Button passengerButton;
-    protected GlobalClass globals;
-    Resources res;
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "75YiknXrR89hvbnIsqK3HUipp";
@@ -30,43 +18,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
-
-        busButton = (Button) findViewById(R.id.busButtonID);
-        passengerButton = (Button) findViewById(R.id.passengerButtonID);
-        globals = (GlobalClass) getApplication();
-        res = getResources();
-
-
-        if (globals.sharedPref.getString(res.getString(R.string.currentStatus), "empty").equals("Bus Driver")) {
-            startActivity(new Intent (this, DriverActivity.class));
-            finish();
-        }
-
-        if (globals.sharedPref.getString(res.getString(R.string.currentStatus), "empty").equals("Passenger")) {
-            startActivity(new Intent(this, RiderActivity.class));
-            finish();
-        }
-
         setContentView(R.layout.activity_main);
-    }
-
-    public void launchDriverActivity(View view) {
-        globals.editor.remove(res.getString(R.string.currentStatus));
-        globals.editor.commit();
-        globals.editor.putString(res.getString(R.string.currentStatus), "Bus Driver");
-        globals.editor.commit();
-        Intent intent = new Intent (this, DriverActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
-    public void launchRiderActivity(View view) {
-        globals.editor.remove(res.getString(R.string.currentStatus));
-        globals.editor.commit();
-        globals.editor.putString(res.getString(R.string.currentStatus), "Passenger");
-        globals.editor.commit();
-        Intent intent = new Intent (this, RiderActivity.class);
-        startActivity(intent);
-        finish();
     }
 }
