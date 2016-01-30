4c2436149178ee0c7fe2312e862894cbf47cc517
AkshayGoel11 <akshaygoel1@gmail.com>
2016-01-30 03:02:03 -0500

    WORKING

--- app/src/main/java/com/hoya/vt/timecheck/MainActivity.java
+++ app/src/main/java/com/hoya/vt/timecheck/MainActivity.java
@@ -3,6 +3,7 @@ package com.hoya.vt.timecheck;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.twitter.sdk.android.Twitter;
@@ -27,12 +28,12 @@ public class MainActivity extends AppCompatActivity {
         Button passengerButton = (Button) findViewById(R.id.passengerButtonID);
     }
 
-    public void launchDriverActivity() {
+    public void launchDriverActivity(View view) {
         Intent intent = new Intent (this, DriverActivity.class);
         startActivity(intent);
     }
 
-    public void launchRiderActivity() {
+    public void launchRiderActivity(View view) {
         Intent intent = new Intent (this, RiderActivity.class);
         startActivity(intent);
     }
