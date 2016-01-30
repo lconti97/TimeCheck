1429c8653a57c42e5256c3b426dd842fe210b5c1
AkshayGoel11 <akshaygoel1@gmail.com>
2016-01-30 05:14:24 -0500

    Revert "App Remembers Selection"
    This reverts commit 9d5b83ca6b3f2c63fe806e239cf5b369485d9773.

--- app/src/main/java/com/hoya/vt/timecheck/MainActivity.java
+++ app/src/main/java/com/hoya/vt/timecheck/MainActivity.java
@@ -1,9 +1,6 @@
 package com.hoya.vt.timecheck;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,11 +12,6 @@ import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button busButton;
-    Button passengerButton;
-    SharedPreferences sharedPref;
-    SharedPreferences.Editor editor;
-
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "75YiknXrR89hvbnIsqK3HUipp";
     private static final String TWITTER_SECRET = "lmUpO2m5m2Rd08klkPWPey4TDQAV9fKpwz7l13yfXrTIvoftcA";
@@ -30,45 +22,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
-
-        busButton = (Button) findViewById(R.id.busButtonID);
-        passengerButton = (Button) findViewById(R.id.passengerButtonID);
-        sharedPref = this.getPreferences(Context.MODE_PRIVATE);
-        editor = sharedPref.edit();
-
-        if (!sharedPref.contains("isBusDriver")) {
-            editor.putBoolean("isBusDriver", false);
-            editor.commit();
-        }
-        if (!sharedPref.contains("isPassenger")) {
-            editor.putBoolean("isPassenger", false);
-            editor.commit();
-        }
-
-        if(sharedPref.getBoolean("isBusDriver", false) == true) {
-            startActivity(new Intent (this, DriverActivity.class));
-        }
-        if (sharedPref.getBoolean("isPassenger", false == true)) {
-            startActivity(new Intent(this, RiderActivity.class));
-        }
-
         setContentView(R.layout.activity_main);
+
+        Button busButton = (Button) findViewById(R.id.busButtonID);
+        Button passengerButton = (Button) findViewById(R.id.passengerButtonID);
     }
 
     public void launchDriverActivity(View view) {
-        editor.remove("isBusDriver");
-        editor.commit();
-        editor.putBoolean("isBusDriver", true);
-        editor.commit();
         Intent intent = new Intent (this, DriverActivity.class);
         startActivity(intent);
     }
 
     public void launchRiderActivity(View view) {
-        editor.remove("isPassenger");
-        editor.commit();
-        editor.putBoolean("isPassenger", true);
-        editor.commit();
         Intent intent = new Intent (this, RiderActivity.class);
         startActivity(intent);
     }
