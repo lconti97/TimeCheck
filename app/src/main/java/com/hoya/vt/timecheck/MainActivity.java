12b7cc7fde2f272e4f1cd7953c31c78a9a62dbea
AkshayGoel11 <akshaygoel1@gmail.com>
2016-01-30 23:35:25 -0500

    activity_main Replaced

--- app/src/main/java/com/hoya/vt/timecheck/MainActivity.java
+++ app/src/main/java/com/hoya/vt/timecheck/MainActivity.java
@@ -1,18 +1,13 @@
 package com.hoya.vt.timecheck;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
 import android.view.View;
-import android.widget.Button;
 
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.tweetcomposer.Card;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -48,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
 
-        setContentView(R.layout.activity_newmain);
+        setContentView(R.layout.activity_main);
     }
 
     public void launchDriverActivity(View view) {
